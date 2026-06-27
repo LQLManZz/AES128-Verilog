f62e8e800e34b843c114909b332b98de59d5f869
Long <long55dbp@gmail.com>
2026-06-27 10:19:33 +0700

    vault backup: 2026-06-27 10:19:33

--- verilog/KeyExpansion/MultiplicativeInv.sv
+++ verilog/KeyExpansion/MultiplicativeInv.sv
@@ -111,7 +111,7 @@ module ModuleX (
 
     output logic [3:0] data_out
 );
-  assign data_out[3] = (data_in1[3] & data_in2[2]) 
+  assign data_out[3] = (data_in1[3] & data_in2[3]) 
                     ^ (data_in1[3] & data_in2[2]) ^ (data_in1[2] & data_in2[3])
                     ^ (data_in1[3] & data_in2[1]) ^ (data_in1[1] & data_in2[3])
                     ^ (data_in1[3] & data_in2[0]) ^ (data_in1[0] & data_in2[3])
@@ -135,20 +135,20 @@ module Inv (
     case (data_in)
       4'h0: data_out = 4'h0;
       4'h1: data_out = 4'h1;
-      4'h2: data_out = 4'h9;
-      4'h3: data_out = 4'he;
-      4'h4: data_out = 4'hc;
-      4'h5: data_out = 4'ha;
-      4'h6: data_out = 4'hd;
+      4'h2: data_out = 4'h3;
+      4'h3: data_out = 4'h2;
+      4'h4: data_out = 4'hf;
+      4'h5: data_out = 4'hc;
+      4'h6: data_out = 4'h9;
       4'h7: data_out = 4'hb;
-      4'h8: data_out = 4'hf;
-      4'h9: data_out = 4'h2;
-      4'ha: data_out = 4'h5;
+      4'h8: data_out = 4'ha;
+      4'h9: data_out = 4'h6;
+      4'ha: data_out = 4'h8;
       4'hb: data_out = 4'h7;
-      4'hc: data_out = 4'h4;
-      4'hd: data_out = 4'h6;
-      4'he: data_out = 4'h3;
-      4'hf: data_out = 4'h8;
+      4'hc: data_out = 4'h5;
+      4'hd: data_out = 4'he;
+      4'he: data_out = 4'hd;
+      4'hf: data_out = 4'h4;
       default: data_out = 4'h0;
     endcase
   end
