b5866dbf67a3bb612af33e8008657e05c04d49b3
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-06-28 06:02:45 +0700

    Implement MUL_For_Inv module for AES decryption

--- RTL/AES_decryption/InvMixColumns/MUL_For_Inv.sv
+++ RTL/AES_decryption/InvMixColumns/MUL_For_Inv.sv
@@ -1,1 +1,12 @@
-
+module MUL_For_Inv(
+    input logic [7:0] Byte,
+    output logic [7:0] Byte_mul9, Byte_mul11, Byte_mul13, Byte_mul14 );
+    logic [7:0] Byte_mul2, Byte_mul4, Byte_mul8;
+    MULX_MixColumns MUL0 (.Byte(Byte), .Byte_mul2(Byte_mul2));
+    MULX_MixColumns MUL1 (.Byte(Byte_mul2), .Byte_mul2(Byte_mul4));
+    MULX_MixColumns MUL2 (.Byte(Byte_mul4), .Byte_mul2(Byte_mul8));
+    assign Byte_mul9 = Byte_mul8 ^ Byte;
+    assign Byte_mul11 = Byte_mul9 ^ Byte_mul2;
+    assign Byte_mul13 = Byte_mul9 ^ Byte_mul4;
+    assign Byte_mul14 = Byte_mul8 ^ Byte_mul4 ^ Byte_mul2;
+endmodule
