27d9ce9c0a404b70eeaab90bfd12153e4d9a741c
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-06-28 06:06:47 +0700

    Implement InvMixColumns module for AES decryption

--- RTL/AES_decryption/InvMixColumns/InvMixColumns.sv
+++ RTL/AES_decryption/InvMixColumns/InvMixColumns.sv
@@ -1,1 +1,11 @@
-
+module InvMixColumns(
+    input logic [127:0] InvMixColumns_in,
+    output logic [127:0] InvMixColumns_out);
+    InvMixCol InvCol0 (.InvCol_in(InvMixColumns_in[127:96]),
+                        .InvCol_out(InvMixColumns_out[127:96]));
+    InvMixCol InvCol1 (.InvCol_in(InvMixColumns_in[95:64]),
+                        .InvCol_out(InvMixColumns_out[95:64]));
+    InvMixCol InvCol2 (.InvCol_in(InvMixColumns_in[63:32]),
+                        .InvCol_out(InvMixColumns_out[63:32]));
+    InvMixCol InvCol3 (.InvCol_in(InvMixColumns_in[31:0]),.InvCol_out(InvMixColumns_out[31:0]));
+endmodule
