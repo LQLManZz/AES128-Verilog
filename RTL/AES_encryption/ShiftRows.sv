7b80f53f17156d43699564716cc0b96e1ef231d4
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-06-28 05:45:17 +0700

    Update ShiftRows.sv

--- RTL/AES_encryption/ShiftRows.sv
+++ RTL/AES_encryption/ShiftRows.sv
@@ -1,1 +1,24 @@
-
+module ShiftRows( 
+    input logic [127:0] ShiftRows_in,
+    output logic [127:0] ShiftRows_out);
+    // row 0
+    assign ShiftRows_out[127:120] = ShiftRows_in[127:120] ;
+    assign ShiftRows_out[95:88] = ShiftRows_in[95:88] ;
+    assign ShiftRows_out[63:56] = ShiftRows_in[63:56] ;
+    assign ShiftRows_out[31:24] = ShiftRows_in[31:24] ;
+    // row 1
+    assign ShiftRows_out[119:112] = ShiftRows_in[87:80] ;
+    assign ShiftRows_out[87:80] = ShiftRows_in[55:48] ;
+    assign ShiftRows_out[55:48] = ShiftRows_in[23:16] ;
+    assign ShiftRows_out[23:16] = ShiftRows_in[119:112] ;
+    // row 2
+    assign ShiftRows_out[111:104] = ShiftRows_in[47:40] ;
+    assign ShiftRows_out[79:72] = ShiftRows_in[15:8] ;
+    assign ShiftRows_out[47:40] = ShiftRows_in[111:104] ;
+    assign ShiftRows_out[15:8] = ShiftRows_in[79:72] ;
+    //row 3
+    assign ShiftRows_out[103:96] = ShiftRows_in[7:0] ;
+    assign ShiftRows_out[71:64] = ShiftRows_in[103:96] ;
+    assign ShiftRows_out[39:32] = ShiftRows_in[71:64] ;
+    assign ShiftRows_out[7:0] = ShiftRows_in[39:32] ;
+endmodule
