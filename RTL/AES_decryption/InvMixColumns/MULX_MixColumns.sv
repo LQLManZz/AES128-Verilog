dcbb65b94d070e385b8d026de1d5c98deb027187
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-06-28 06:00:42 +0700

    Update MULX_MixColumns.sv

--- RTL/AES_decryption/InvMixColumns/MULX_MixColumns.sv
+++ RTL/AES_decryption/InvMixColumns/MULX_MixColumns.sv
@@ -1,1 +1,7 @@
-
+module MULX_MixColumns( 
+    input logic [7:0] Byte,
+    output logic [7:0] Byte_mul2);
+    logic [7:0] ByteSL;
+    assign ByteSL = Byte << 1;
+    assign Byte_mul2 = Byte[7] ? (ByteSL ^ 8'h1b) : ByteSL;
+endmodule
