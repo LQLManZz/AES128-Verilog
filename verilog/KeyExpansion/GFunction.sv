90ab30c4c8beedee075109167346d434c084fda7
Long <long55dbp@gmail.com>
2026-06-27 18:49:57 +0700

    vault backup: 2026-06-27 18:49:57

---
+++ verilog/KeyExpansion/GFunction.sv
@@ -0,0 +1,23 @@
+module GFunction (
+    input logic [31:0] word_in,
+    input logic [ 3:0] round_index,
+
+    output logic [31:0] word_out
+);
+  logic [31:0] after_rotword;
+  logic [31:0] after_subword;
+
+  RotWord rw1 (
+      .word_in (word_in),
+      .word_out(after_rotword)
+  );
+  SubWord sw1 (
+      .word_in (after_rotword),
+      .word_out(after_subword)
+  );
+  AddRcon arc1 (
+      .word_in(after_subword),
+      .round_index(round_index),
+      .word_out(word_out)
+  );
+endmodule
