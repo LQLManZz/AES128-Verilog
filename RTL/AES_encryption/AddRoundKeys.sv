81d040411a06fc4309c925678e410de541534743
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-06-28 05:43:10 +0700

    Implement AddRoundKeys module for AES encryption

--- RTL/AES_encryption/AddRoundKeys.sv
+++ RTL/AES_encryption/AddRoundKeys.sv
@@ -1,1 +1,5 @@
-
+module AddRoundKeys(
+    input logic  [127:0] RoundKey, AddRoundKeys_in,
+    output logic [127:0] AddRoundKeys_out);
+    assign AddRoundKeys_out = AddRoundKeys_in ^ RoundKey;
+endmodule
