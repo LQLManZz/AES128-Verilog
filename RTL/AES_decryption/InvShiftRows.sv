9fa694ac0a70a1265ecd3ee57674f5d45d14aed3
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-06-28 05:57:33 +0700

    Update InvShiftRows.sv

--- RTL/AES_decryption/InvShiftRows.sv
+++ RTL/AES_decryption/InvShiftRows.sv
@@ -1,1 +1,24 @@
-
+module InvShiftRows(
+    input logic [127:0] InvShiftRows_in,
+    output logic [127:0] InvShiftRows_out);
+    //row 0
+    assign InvShiftRows_out[127:120] = InvShiftRows_in[127:120] ;
+    assign InvShiftRows_out[95:88] = InvShiftRows_in[95:88] ;
+    assign InvShiftRows_out[63:56] = InvShiftRows_in[63:56] ;
+    assign InvShiftRows_out[31:24] = InvShiftRows_in[31:24] ;
+    //row 1
+    assign InvShiftRows_out[119:112] = InvShiftRows_in[23:16] ;
+    assign InvShiftRows_out[87:80] = InvShiftRows_in[119:112] ;
+    assign InvShiftRows_out[55:48] = InvShiftRows_in[87:80] ;
+    assign InvShiftRows_out[23:16] = InvShiftRows_in[55:48] ;
+    //row 2
+    assign InvShiftRows_out[111:104] = InvShiftRows_in[47:40] ;
+    assign InvShiftRows_out[79:72] = InvShiftRows_in[15:8] ;
+    assign InvShiftRows_out[47:40] = InvShiftRows_in[111:104] ;
+    assign InvShiftRows_out[15:8] = InvShiftRows_in[79:72] ;
+    //row 3
+    assign InvShiftRows_out[103:96] = InvShiftRows_in[71:64] ;
+    assign InvShiftRows_out[71:64] = InvShiftRows_in[39:32] ;
+    assign InvShiftRows_out[39:32] = InvShiftRows_in[7:0] ;
+    assign InvShiftRows_out[7:0] = InvShiftRows_in[103:96] ;
+endmodule
