c3bd7e2c9ba30a809950dab0f57d26018714d4ac
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-07-12 19:46:40 +0700

    Update Write_pointer.sv

--- RTL/FIFO/Write_pointer.sv
+++ RTL/FIFO/Write_pointer.sv
@@ -1,1 +1,13 @@
-
+module Write_pointer #(
+    parameter int PTR_WIDTH = 4)
+    (input logic clk, rst_n, w_en, r_en, full,
+    output logic [PTR_WIDTH:0] w_ptr);
+    logic w_inc;
+    assign w_inc = w_en&(~full|r_en);
+    always_ff @ (posedge clk or negedge rst_n) begin
+        if(!rst_n)
+            w_ptr <= '0;
+        else if (w_inc) 
+            w_ptr <= w_ptr+1'b1;
+    end
+endmodule
