88c0746036e4045470a89f431ad43e736130a795
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-07-15 19:03:23 +0700

    Update AES_register.sv

--- RTL/AES_encryption/AES_register.sv
+++ RTL/AES_encryption/AES_register.sv
@@ -1,16 +1,21 @@
 module AES_register(
-    input logic clk,rst_n, Valid_in,
+    input logic clk,rst_n,
+    input logic [1:0] data_type_in,
     input logic [127:0] data_in,
     output logic [127:0] data_out,
-    output logic Valid_out);
+    output logic [1:0] data_type_out,
+    output logic valid);
+    
+    assign valid = (data_type_in!= 2'b0);
+    
     always_ff @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             data_out <= 128'b0;
-            Valid_out<= 1'b0;
+            data_type_out <= 2'b0;
         end    
         else begin
-            Valid_out <= Valid_in;
-            if (Valid_in)
+            data_type_out <= data_type_in;
+            if (valid)
                 data_out <= data_in;
         end
     end
