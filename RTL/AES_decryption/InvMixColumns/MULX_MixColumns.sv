48f9235b2fe353306e97707bfe648a060dc28e6b
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-08-31 05:14:22 +0700

    Delete RTL/AES_decryption directory

--- RTL/AES_decryption/InvMixColumns/MULX_MixColumns.sv
+++
@@ -1,7 +0,0 @@
-module MULX_MixColumns( 
-    input logic [7:0] Byte,
-    output logic [7:0] Byte_mul2);
-    logic [7:0] ByteSL;
-    assign ByteSL = Byte << 1;
-    assign Byte_mul2 = Byte[7] ? (ByteSL ^ 8'h1b) : ByteSL;
-endmodule
