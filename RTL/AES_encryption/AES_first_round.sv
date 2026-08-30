9227078a0d67dbb7d75ae717449585203192124c
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-08-31 05:21:51 +0700

    Update AES_first_round.sv

--- RTL/AES_encryption/AES_first_round.sv
+++ RTL/AES_encryption/AES_first_round.sv
@@ -1,1 +1,15 @@
-
+module AES_first_round(
+    input logic clk,rst_n,
+    input logic [1:0] data_type_in,
+    input logic [127:0] data_in,
+    input logic [127:0] round_key,
+    output logic [127:0] data_out,
+    output logic [1:0] data_type_out,
+    output logic valid);
+    
+    logic [127:0] register_out;
+    AES_register AES_reg (.clk(clk),.rst_n(rst_n),.data_type_in(data_type_in),.data_in(data_in),
+                    .data_out(register_out),.data_type_out(data_type_out),.valid(valid));
+    AddRoundKeys ARK (.RoundKey(round_key),.AddRoundKeys_in(register_out),
+                    .AddRoundKeys_out(data_out));
+endmodule
