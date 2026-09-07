06a733da5b1a252ed487557e9a05f9b005d6da98
lequocviet24VDT <khanhviet03030506@gmail.com>
2026-09-08 05:17:06 +0700

    Create AES_256_CTR.sv

---
+++ RTL/AES_256_CTR/AES_256_CTR.sv
@@ -0,0 +1,101 @@
+module AES_256_CTR(
+    input logic clk, rst_n, finish_reset,
+    input logic load_key, load_data, load_IV,
+    input logic [127:0] data_in,
+    input logic [255:0] cipher_key,
+    input logic [95:0] IV,
+    input logic expansion_en, mode,
+    input logic data_in_last,
+    output logic AES_finish, CTR_counter_overflow,
+    output logic IV_loaded, expansion_finish,
+    output logic [127:0] H, E, data_out, CT,
+    output logic data_out_last,
+    output logic H_valid, E_valid, data_valid,
+    output logic CT_last, CT_valid);
+    
+    logic gen_J0;
+    always_ff @(posedge clk or negedge rst_n) begin
+        if(!rst_n)
+            gen_J0 <= 1'b0;
+        else begin
+            gen_J0 <= data_in_last;
+        end
+    end
+    
+    logic data_req;
+    logic [128:0] FIFO_data_in;
+    logic [128:0] FIFO_data;
+    assign FIFO_data_in = {data_in_last,data_in};
+    FIFO #(.DATA_SIZE(129),.DEPTH(16)) fifo 
+            (.clk(clk),.rst_n(rst_n),.data_in(FIFO_data_in),.w_en(load_data),.r_en(data_req),
+            .data_out(FIFO_data));
+    logic [127:0] round_key [0:14];
+    KeyExpansion_256 key_expan (.clk(clk),.rst_n(rst_n),.expansion_en(expansion_en),
+                            .cipher_key(cipher_key),.round_key(round_key),
+                            .expansion_finish(expansion_finish));
+    logic [127:0] AES_data_in;
+    logic [1:0] data_type_in;
+    counter_generator count_gen (.clk(clk),.rst_n(rst_n),.finish_reset(finish_reset),
+                                .gen_J0(gen_J0),.load_key(load_key),.load_data(load_data),
+                                .load_IV(load_IV),.IV(IV),.data_type(data_type_in),
+                                .data_out(AES_data_in),.IV_loaded(IV_loaded),
+                                .CTR_counter_overflow(CTR_counter_overflow));
+    logic [127:0] AES_data_out;
+    logic [1:0] data_type_out;
+    AES_256_encryption AES (.clk(clk),.rst_n(rst_n),.data_in(AES_data_in),.round_key(round_key),
+                        .data_type_in(data_type_in),.data_out(AES_data_out),
+                        .data_type_out(data_type_out),.data_req(data_req),
+                        .AES_finish(AES_finish));
+    logic [127:0] keystream;
+    logic keystream_valid;
+    always_comb begin
+        H = 128'b0;
+        E = 128'b0;
+        keystream = 128'b0;
+        H_valid = 1'b0;
+        keystream_valid = 1'b0;
+        E_valid = 1'b0;
+        case(data_type_out)
+            2'b01: begin
+                H = AES_data_out;
+                H_valid = 1'b1;
+            end
+            2'b10: begin
+                keystream = AES_data_out;
+                keystream_valid = 1'b1;
+            end
+            2'b11: begin
+                E = AES_data_out;
+                E_valid = 1'b1;
+            end
+            default: begin
+                H                = 128'b0;
+                E                = 128'b0;
+                keystream        = 128'b0;
+                H_valid          = 1'b0;
+                keystream_valid  = 1'b0;
+                E_valid          = 1'b0;
+            end
+        endcase
+    end
+    
+    always_ff @(posedge clk or negedge rst_n) begin: outReg
+        if(!rst_n) begin
+            data_valid    <= 1'b0;
+            data_out_last <= 1'b0;
+            data_out      <= 128'b0;
+        end
+        else begin
+            data_valid <= keystream_valid;
+            data_out_last <= FIFO_data[128] & keystream_valid;
+            if (keystream_valid)
+                data_out <= FIFO_data[127:0] ^ keystream;
+            else
+                data_out <= 128'b0;
+        end
+    end
+
+    assign CT = mode? FIFO_data [127:0] : (FIFO_data [127:0] ^ keystream); 
+    assign CT_valid = keystream_valid;
+    assign CT_last  = keystream_valid & FIFO_data[128];
+endmodule
